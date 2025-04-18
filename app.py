f00b0f870136420d4dd67a11afeb2a6ffed50dc0
Ruffhouse25 <rruff25@gmail.com>
2025-04-17 21:11:09 -0400

    add model file and update requirements

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import logging
 import sys
 from forms import VenueForm, ArtistForm, ShowForm
+from models import db, Artist, Venue, Show
+
 
 # Initialize Flask app, database, and migration
 app = Flask(__name__)
@@ -12,50 +14,6 @@ app.config.from_object('config')  # Ensure config.py has your DB URI
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Models
-
-class Venue(db.Model):
-    __tablename__ = 'venues'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    facebook_link = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='venue', lazy=True, cascade="all, delete-orphan")
-
-
-class Artist(db.Model):
-    __tablename__ = 'artists'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    facebook_link = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='artist', lazy=True, cascade="all, delete-orphan")
-
-
-class Show(db.Model):
-    __tablename__ = 'shows'
-
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 # Custom Jinja Filter
 @app.template_filter('datetime')
